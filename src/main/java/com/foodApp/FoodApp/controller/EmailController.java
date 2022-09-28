fb7679c496312f4e9bdeb893688e0650dbed7708
GauravTyagi007 <gtyagi200006@gmail.com>
2022-09-28 10:24:23 +0530

    backend done with Swagger

--- src/main/java/com/foodApp/FoodApp/controller/EmailController.java
+++ src/main/java/com/foodApp/FoodApp/controller/EmailController.java
@@ -5,9 +5,11 @@ import com.foodApp.FoodApp.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/email")
 public class EmailController {
 
     @Autowired
